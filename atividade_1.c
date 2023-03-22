e6ea2035f80f3471f97f37927a4e00f0b001d9ce
Filipe <128107143+Filipecorr34@users.noreply.github.com>
2023-03-21 23:40:18 -0300

    Update atividade_1.c

--- atividade_1.c
+++ atividade_1.c
@@ -1,7 +1,11 @@
 int main()
 {
     printf("Aluno: Paulo Vinicius de Freitas Rodrigues \n");
-    printf("Matricula: 202206840037");
+    printf("Matricula: 202206840037 \n");
+
+    printf("Aluno: Filipe Correa da Silva \n");
+    printf("Matricula: 202006840020 \n");
+
     return 0;
-    
+
 }
