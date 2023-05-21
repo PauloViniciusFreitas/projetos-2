9d4e9d59fba659039de85e73fdbe991809555d17
ViniciusFreitas <102534497+Vinnie00Freitas@users.noreply.github.com>
2023-05-20 22:53:07 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "matrizes.h"
+#include <stdio.h>
+#include <stdlib.h>
+    
+int main(void) {
+    printf("=====Equipe=====\n");
+    printf("Filipe Correa da Silva\n");
+    printf("Paulo Vinicius de Freitas Rodrigues\n");
+    printf("Kelton de Jesus Freitas Cantao\n");
+    printf("\n");
+    teste_todos();
+    return 0;
+}
